a79cdfd994d41ab5421f16a7ffe001ecf8ce7964
kylefickiotsatech <130463756+kylefickiotsatech@users.noreply.github.com>
2024-07-31 15:34:47 +0200

    Upgrade

---
+++ RachetCoin/RachetCoin/Controllers/BlockchainController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using RachetCoinUIModels.Models;
+using System.Collections.Generic;
+
+namespace RachetCoin.Controllers
+{
+    public class BlockchainController : Controller
+    { 
+        private static Blockchain blockchain = new Blockchain();
+        public IActionResult Index()
+        {
+            return View(_blockchain);
+        }
+
+        public IActionResult Mine()
+        {
+            var lastBlock = _blockchain.GetLastBlock();
+            var transactions = new List<Transaction>
+            {
+                new Transaction("network", "miner-address", 1)
+            };
+            var newBlock = new Block(DateTime.Now, transactions, lastBlock.Hash);
+            _blockchain.AddBlock(newBlock);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult AddTransaction(string fromAddress, string toAddress, decimal amount)
+        {
+            var transaction = new Transaction(fromAddress, toAddress, amount);
+            var transactions = new List<Transaction> { transaction };
+            var lastBlock = _blockchain.GetLatestBlock();
+            var newBlock = new Block(DateTime.Now, transactions, lastBlock.Hash);
+            _blockchain.AddBlock(newBlock);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ RachetCoin/RatchetCoinUIModels/Models/Block.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace RatchetCoinUIModels.Models
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        public DateTime Timestamp { get; set; }
+        public IList<Transaction> Transactions { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public int Nonce { get; set; }
+        public Block(DateTime timestamp, IList<Transaction> transactions, string previousHash = "")
+        {
+            Index = 0;
+            Timestamp = timestamp;
+            Transactions = transactions;
+            PreviousHash = previousHash;
+            Hash = CalculateHash();
+            Nonce = 0;
+        }
+
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{Timestamp}-{PreviousHash ?? string.Empty}-{Nonce}-{Transactions}");
+            byte[] outputBytes = sha256.ComputeHash( inputBytes );
+            return Convert.ToBase64String( outputBytes );
+        }
+
+        public void MineBlock(int difficulty)
+        {
+            string hashValidationTemplate = new string('0', difficulty);
+            while (Hash.Substring(0, difficulty ) != hashValidationTemplate)
+            {
+                Nonce++;
+                Hash = CalculateHash();
+            }
+        }
+    }
+}
